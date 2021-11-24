3be7cdfc704ec418deaaec3c2e89983d26613e21
Tang Peng <derrick.tang@miniso.com>
2021-11-24 17:59:10 +0800

    first commit

---
+++ src/main/java/com/tangp/sourcingsearch/SourcingSearchApplication.java
@@ -0,0 +1,13 @@
+package com.tangp.sourcingsearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SourcingSearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SourcingSearchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tangp/sourcingsearch/SourcingSearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tangp.sourcingsearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SourcingSearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
